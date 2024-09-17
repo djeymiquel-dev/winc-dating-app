b516765e3bd96937ca5a3cc8f7c55ebd220db38a
djeymiquel-dev <djey.miquel.developer@gmail.com>
2024-09-17 03:22:59 +0200

    modified and ready to commit to winc

--- winc_dating_project.js
+++ winc_dating_project.js
@@ -2,16 +2,16 @@
 const mockData = require("./mockData").data;
 const prompt = require("prompt-sync")();
 
-//Gebruik dit om testen
-// const userProfileMock = {
-//   first_name: "djey",
-//   last_name: "pocorni",
-//   age: 36,
-//   gender: "M",
-//   gender_interest: "F",
-//   location: "city",
-//   min_age_interest: 25,
-//   max_age_interest: 60,
+// //Use this to test if everything is working correctly
+// const userProfile = {
+//   first_name: "",
+//   last_name: "",
+//   age: ,
+//   gender: "",
+//   gender_interest: "",
+//   location: "",
+//   min_age_interest: ,
+//   max_age_interest: ,
 // };
 
 // User Profile
@@ -19,94 +19,135 @@ const userProfile = {};
 
 // Questions for creating a profile
 const questions = [
-  "What is your first name? ",
-  "What is your last name? ",
+  "What is your First name? ",
+  "and your Last name? ",
   "What is your age? ",
-  "What is your gender (M/F/X)? ",
-  "Which gender are you interested in (M/F/X)? ",
-  "What is your location (city/rural)? ",
-  "What is the minimum age you are interested in? ",
-  "What is the maximum age you are interested in? ",
+  "What is your gender identity? (M/F/X) ",
+  "Which gender are you interested in? (M/F/X) ",
+  "What is your location? (city/rural) ",
+  "Could you please specify the minimum age you are interested in? ",
+  "And the maximum age? ",
 ];
 
-let i = 0; // Begin bij de eerste vraag (index [0])
+let i = 0; // Starts question @ (index [0])
 while (i < questions.length) {
   let answer = prompt(questions[i].trim());
-  const capitalizeFirstLetter =
-    answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase();
 
-  // Sla het antwoord op in het userProfile object op basis van de vraag
-  // Controleer of de input geldig is
-  // Vraag dezelfde vraag opnieuw als de input ongeldig is
+  // variables for evaluating valid inputs
+  const notAllowedInput = "0123456789".split("");
+  const capitalizedName =
+    answer.split("", 1).join().toUpperCase() + answer.slice(1).toLowerCase();
+  const emptyString = "";
+  const genderCapitalized = answer.toUpperCase();
+  const genderInterestCapitalized = answer.toUpperCase();
+  const age = Number(answer);
+  const minAge = Number(answer);
+  const maxAge = Number(answer);
 
+  // Store answer based on the given question
+  // Check if input is valid
+  // Prompt question again if answer is invalid
+  let inputIsValid = true;
   switch (i) {
     case 0:
-      if (answer === "") {
-        continue;
+      // check if answer includes numbers
+      for (let char of answer) {
+        if (notAllowedInput.includes(char)) {
+          inputIsValid = false;
+          break;
+        }
       }
-      userProfile.firstName = capitalizeFirstLetter;
-      break;
+      if (!inputIsValid) {
+        continue;
+      } else if (answer === emptyString) {
+        continue;
+      } else {
+        userProfile.first_name = capitalizedName;
+        break;
+      }
 
     case 1:
-      if (answer === "") {
-        continue;
+      for (let char of answer) {
+        if (notAllowedInput.includes(char)) {
+          inputIsValid = false;
+          break;
+        }
       }
-      userProfile.lastName = capitalizeFirstLetter;
-      break;
+      if (!inputIsValid) {
+        continue;
+      } else if (answer === emptyString) {
+        continue;
+      } else {
+        userProfile.lastName = capitalizedName;
+        break;
+      }
 
     case 2:
-      let age = Number(answer);
       if (isNaN(age) || age < 18) {
         continue;
+      } else {
+        userProfile.age = age;
+        break;
       }
-      userProfile.age = age;
-      break;
 
     case 3:
-      if (answer !== "m" && answer !== "f" && answer !== "x") {
+      if (
+        genderCapitalized !== "M" &&
+        genderCapitalized !== "F" &&
+        genderCapitalized !== "X"
+      ) {
         continue;
+      } else {
+        userProfile.gender = genderCapitalized;
+        break;
       }
-      userProfile.gender = answer.toUpperCase();
-      break;
 
     case 4:
-      if (answer !== "m" && answer !== "f" && answer !== "x") {
+      if (
+        genderInterestCapitalized !== "M" &&
+        genderInterestCapitalized !== "F" &&
+        genderInterestCapitalized !== "X"
+      ) {
         continue;
+      } else {
+        userProfile.gender_interest = genderInterestCapitalized;
+        break;
       }
-      userProfile.gender_interest = answer.toUpperCase();
-      break;
 
     case 5:
       if (answer !== "city" && answer !== "rural") {
         continue;
+      } else {
+        userProfile.location = answer;
+        break;
       }
-      userProfile.location = answer;
-      break;
 
     case 6:
-      let minAge = Number(answer);
       if (isNaN(minAge) || minAge < 18) {
         continue;
+      } else {
+        userProfile.min_age_interest = minAge;
+        break;
       }
-      userProfile.min_age_interest = minAge;
-      break;
 
     case 7:
-      let maxAge = Number(answer);
       if (
         isNaN(maxAge) ||
         maxAge < 18 ||
-        maxAge < userProfile.min_age_interest // Controleer of de max_age_interest hoger is dan de min_age_interest
+        maxAge < userProfile.min_age_interest // Check if the max_age_interest is not < less than min_age_interest (max_age:30 < min_:32 ?)
       ) {
         continue;
+      } else {
+        userProfile.max_age_interest = maxAge;
+        break;
       }
-      userProfile.max_age_interest = maxAge;
-      break;
   }
 
-  i++; // Ga door naar de volgende vraag als de huidige vraag correct is beantwoord
+  i++; // Go to next question if answer is correct
 }
 
+// Check if there is a match based on the criteria
+// Count the number of matches found
 let matchResults = [];
 let numberOfMatches = 0;
 
@@ -133,5 +174,10 @@ for (let datingMatch of mockData) {
   }
 }
 
-console.log(`You've got ${numberOfMatches} matches!`);
+const heart = "❤️";
+if (numberOfMatches === 1) {
+  console.log(`You have ${numberOfMatches} match ${heart} !`);
+} else {
+  console.log(`You have ${numberOfMatches} matches${heart} !`);
+}
 console.table(matchResults);
